00969ba66470488db82aed48f039432cb9a36481
mosse <mjemacdonald@gmail.com>
2015-03-07 16:45:34 -0800

    Implements correct spinner display and shorten input filters

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,11 +3,15 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, Links, $location) {
   // Your code here
   $scope.link = {};
+  $scope.loading = false;
 
   $scope.addLink = function() {
+    $scope.loading = true;
     Links.saveLink($scope.link)
       .then(function(){
         console.log("ng module says: link shortened");
+        $scope.loading = false;
+        // $location.path('/');
       })
       .catch(function (error) {
         console.error(error);
